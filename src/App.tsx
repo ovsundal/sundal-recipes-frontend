79305aee89cf23c6208d7209ab18221c97a27da8
osund <osundal83@gmail.com>
2020-03-20 23:59:18 +0100

    add api call for getting recipes and create recipe boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
 import * as React from 'react'
+import styled from "styled-components";
+import {useEffect, useState} from "react";
 
+interface IRecipe {
+    ingredients: string[],
+    title: string,
+    instructions: string,
+    id: string
+}
 
 export const App: React.FC = () => {
 
+    const [recipeData, setRecipeData] = useState([] as IRecipe[]);
+
+    useEffect(() => {
+        const fetchRecipes = async() => {
+          try {
+              const response = await fetch('https://sundal-recipes.herokuapp.com/api/recipes/getRecipes');
+
+              const {recipes}: {recipes: IRecipe[]} = await response.json();
+
+              setRecipeData(recipes);
+          } catch (e) {
+              console.log('Error, could not get recipes', e);
+          }
+
+        };
+
+        fetchRecipes();
+    }, []);
+
+
     return(
-        <h1>app</h1>
+        <AppWrapper>
+            {recipeData.map(recipeItem)}
+        </AppWrapper>
     )
 };
+
+const recipeItem = (recipe: IRecipe) => {
+    const {id, title, ingredients, instructions} = recipe;
+    return (
+        <div key={id}>
+            <h1>{title}</h1>
+            <p>{ingredients}</p>
+            <p>{instructions}</p>
+        </div>
+    )
+};
+
+const AppWrapper = styled.div`
+  display: flex;
+`;
