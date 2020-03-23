d3af505c9782e94838f9913b5e413f4297a52fda
osund <osundal83@gmail.com>
2020-03-23 22:02:11 +0100

    add states for title, ingredients and instructions

--- src/recipes/AddRecipe.tsx
+++ src/recipes/AddRecipe.tsx
@@ -1,24 +1,58 @@
-import * as React from 'react'
+import * as React from "react";
 import styled from "styled-components";
+import { useState } from "react";
 
-interface IAddRecipeProps {
-
-}
+interface IAddRecipeProps {}
 
 export const AddRecipe: React.FC<IAddRecipeProps> = ({}) => {
+  const [title, setTitle] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
 
+  const updateFieldValue = (e: React.FormEvent<HTMLInputElement>) => {
+    const { value, id } = e.currentTarget;
 
+    if (id === "recipe-title") {
+      setTitle(value);
+    } else if (id === "recipe-ingredients") {
+      setIngredients(value);
+    } else if (id === "recipe-instructions") {
+      setInstructions(value);
+    }
+  };
 
-    return(
-        <AddRecipeWrapper>
-            <label>
-                Title
-                <input type={'text'} />
-            </label>
-        </AddRecipeWrapper>
-    )
-}
+  return (
+    <AddRecipeWrapper>
+      <h1>Add Recipe</h1>
+      <label>
+        Title
+        <input
+          id={"recipe-title"}
+          type={"text"}
+          value={title}
+          onChange={updateFieldValue}
+        />
+      </label>
+      <label>
+        Ingredients
+        <input
+          id={"recipe-ingredients"}
+          type={"text"}
+          value={ingredients}
+          onChange={updateFieldValue}
+        />
+      </label>
+      <label>
+        instructions
+        <input
+          id={"recipe-instructions"}
+          type={"text"}
+          value={instructions}
+          onChange={updateFieldValue}
+        />
+      </label>
+    </AddRecipeWrapper>
+  );
+};
 
-const AddRecipeWrapper = styled.form`
-
-`;
+const AddRecipeWrapper = styled.form``;
