1a9ea3653ba498d7a2d284db7e2097f6015b648e
osund <osundal83@gmail.com>
2020-03-23 21:52:13 +0100

    move fab to recipelist

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,25 @@
-import * as React from 'react'
+import * as React from "react";
 import styled from "styled-components";
-import {useEffect, useState} from "react";
-import {IRecipe, recipeItem} from "./recipes/RecipeItem";
-import {RecipeList} from "./recipes/RecipeList";
+import {AppRoutes} from "./AppRoutes";
 
 export const App: React.FC = () => {
 
-    const [recipeData, setRecipeData] = useState([] as IRecipe[]);
 
-    useEffect(() => {
-        const fetchRecipes = async() => {
-          try {
-              const response = await fetch('https://sundal-recipes.herokuapp.com/api/recipes/getRecipes');
-
-              const {recipes}: {recipes: IRecipe[]} = await response.json();
-
-              setRecipeData(recipes);
-          } catch (e) {
-              console.log('Error, could not get recipes', e);
-          }
-
-        };
-
-        fetchRecipes();
-    }, []);
-
-
-    return(
-        <AppWrapper>
-            <RecipeList data={recipeData} />
-
-            <FloatingAddRecipeButton href={'/add-recipe'} title={'Add Recipe'}>
-                <PlusIcon className="fa fa-plus my-float" />
-            </FloatingAddRecipeButton>
-        </AppWrapper>
-    )
+  return (
+    <AppWrapper>
+      <AppRoutes />
+    </AppWrapper>
+  );
 };
+
 
 const AppWrapper = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
   align-items: center;
-  
-  font-family:Verdana, Geneva, sans-serif;
-  font-size:18px;
-  background-color:#CCC;
-`;
 
-const FloatingAddRecipeButton = styled.a`
-    position:fixed;
-    width:60px;
-    height:60px;
-    bottom:40px;
-    right:40px;
-    background-color:#0C9;
-    color:#FFF;
-    border-radius:50px;
-    text-align:center;
-    box-shadow: 2px 2px 3px #999;
-`;
-
-const PlusIcon = styled.i`
-  margin-top:22px;
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 18px;
+  background-color: #ccc;
 `;
--- src/recipes/RecipeList.tsx
+++ src/recipes/RecipeList.tsx
@@ -1,22 +1,65 @@
 import * as React from 'react'
 import {IRecipe, recipeItem} from "./RecipeItem";
 import styled from "styled-components";
+import {BrowserRouter, Link} from "react-router-dom";
+import {useState} from "react";
+import {useEffect} from "react";
 
 interface IRecipeListProps {
-    data: IRecipe[]
 }
 
-export const RecipeList: React.FC<IRecipeListProps> = ({data}) => {
+export const RecipeList: React.FC<IRecipeListProps> = ({}) => {
 
+    const [recipeData, setRecipeData] = useState([] as IRecipe[]);
 
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const response = await fetch(
+                    "https://sundal-recipes.herokuapp.com/api/recipes/getRecipes"
+                );
+
+                const { recipes }: { recipes: IRecipe[] } = await response.json();
+
+                setRecipeData(recipes);
+            } catch (e) {
+                console.log("Error, could not get recipes", e);
+            }
+        };
+
+        fetchRecipes();
+    }, []);
 
     return(
         <RecipeListWrapper>
-            {data.map(recipeItem)}
+            {recipeData.map(recipeItem)}
+
+            <Link to={"/add-recipe"}>
+                <FloatingAddRecipeButton title={"Add Recipe"}>
+                    <PlusIcon className="fa fa-plus my-float" />
+                </FloatingAddRecipeButton>
+            </Link>
         </RecipeListWrapper>
     )
 };
 
 const RecipeListWrapper = styled.div`
 
+`;
+
+const FloatingAddRecipeButton = styled.a`
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 40px;
+  right: 40px;
+  background-color: #0c9;
+  color: #fff;
+  border-radius: 50px;
+  text-align: center;
+  box-shadow: 2px 2px 3px #999;
+`;
+
+const PlusIcon = styled.i`
+  margin-top: 22px;
 `;
