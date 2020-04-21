29b6b8bc995f07d022566b7346e8478ffed6792d
osund <osundal83@gmail.com>
2020-04-21 22:06:10 +0200

    add route to view recipe item

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,23 +1,17 @@
-import * as React from 'react'
-import {Switch, Route, BrowserRouter} from 'react-router-dom'
-import {AddRecipe} from "./recipes/AddRecipe";
-import {RecipeList} from "./recipes/RecipeList";
-
+import * as React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { AddRecipe } from "./recipes/AddRecipe";
+import { RecipeList } from "./recipes/RecipeList";
+import { RecipeItem } from "./recipes/RecipeItem";
 
 export const AppRoutes: React.FC = () => {
-
-
-
-    return(
-        <BrowserRouter>
-        <Switch>
-            <Route exact={true} path={"/"}>
-                <RecipeList />
-            </Route>
-            <Route path={"/add-recipe"}>
-                <AddRecipe />
-            </Route>
-        </Switch>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path={"/"} children={<RecipeList />} />
+        <Route exact={true} path={"/:recipeName"} children={<RecipeItem />} />
+        <Route path={"/add-recipe"} children={<AddRecipe />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
