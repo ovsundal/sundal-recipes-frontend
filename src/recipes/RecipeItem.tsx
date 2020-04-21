9fdce19d414fb5b1ee5ac0e640186e45e843a5df
osund <osundal83@gmail.com>
2020-04-21 23:12:40 +0200

    get recipe body when clicking on an item in recipe list

--- src/recipes/RecipeItem.tsx
+++ src/recipes/RecipeItem.tsx
@@ -1,15 +1,37 @@
 import * as React from "react";
 import styled from "styled-components";
 import sanitize from "sanitize-html";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export interface IRecipe {
-  ingredients: string[];
-  title: string;
-  instructions: string;
   id: string;
+  recipe: string;
+  title: string;
 }
 
-export const recipeItem = (recipe: any) => {
+export const RecipeItem = () => {
+  const [recipe, setRecipe] = useState({} as IRecipe);
+  const { recipeId } = useParams();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await fetch(
+          `https://sundal-recipes.herokuapp.com/api/recipes/getRecipe?id=${recipeId}`
+          // `http://localhost:5000/api/recipes/getRecipe?id=${recipeId}`
+        );
+        const { recipes }: { recipes: IRecipe[] } = await response.json();
+
+        setRecipe(recipes[0]);
+      } catch (e) {
+        console.log("Error, could not get recipes", e);
+      }
+    };
+
+    fetchRecipe();
+  }, [recipeId]);
+
   const { recipe: content, id, title } = recipe;
   const sanitizedContent = sanitize(content);
 
