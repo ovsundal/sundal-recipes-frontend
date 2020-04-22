1c69117d80c36e30d2a29e43dacf6b35d7b27e37
osund <osundal83@gmail.com>
2020-04-22 22:38:48 +0200

    add update recipe route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { AddRecipe } from "./recipes/AddRecipe";
 import { RecipeList } from "./recipes/RecipeList";
 import { RecipeItem } from "./recipes/RecipeItem";
+import { UpdateRecipe } from "./recipes/UpdateRecipe";
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -15,6 +16,11 @@ export const AppRoutes: React.FC = () => {
           children={<RecipeItem />}
         />
         <Route exact={true} path={"/add-recipe"} children={<AddRecipe />} />
+        <Route
+          exact={true}
+          path={"/update-recipe"}
+          children={<UpdateRecipe />}
+        />
       </Switch>
     </BrowserRouter>
   );
