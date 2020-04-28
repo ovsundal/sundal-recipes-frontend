5a3420395e2b5e84187afcc65006749df1ec7548
osund <osundal83@gmail.com>
2020-04-28 19:30:57 +0200

    center spinner on screen

--- src/common/Spinner.tsx
+++ src/common/Spinner.tsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const Spinner = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+
   color: black;
   font-size: 20px;
-  margin: 100px auto;
   width: 1em;
   height: 1em;
   border-radius: 50%;
-  position: relative;
   text-indent: -9999em;
   -webkit-animation: load4 1.3s infinite linear;
   animation: load4 1.3s infinite linear;
