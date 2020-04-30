240be1ae56a52d2cfbc5e062a22cb1c687540173
osund <osundal83@gmail.com>
2020-04-30 09:53:05 +0200

    change endpoint

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,2 @@
-// export const GLOBAL_API = "https://sundal-recipes.herokuapp.com/api/";
-export const GLOBAL_API = "http://localhost:5000/api/";
+export const GLOBAL_API = "https://sundal-recipes.herokuapp.com/api/";
+// export const GLOBAL_API = "http://localhost:5000/api/";
