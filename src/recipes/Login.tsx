f7e026461db7bdac6bb0175cc892912747a897a4
osund <osundal83@gmail.com>
2020-08-10 21:47:21 +0200

    build out login component

--- src/recipes/Login.tsx
+++ src/recipes/Login.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 export const Login = () => {
-  return <h1>Login screen</h1>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const submitForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // TODO: create backend endpoint and do auth
+    console.log("clicked form submit");
+  };
+
+  return (
+    <LoginWrapper onSubmit={submitForm}>
+      Login screen. User is logged in: {isLoggedIn ? "true" : "false"}
+      <label>
+        Username
+        <input />
+      </label>
+      <label>
+        Password
+        <input />
+      </label>
+      <button type={"submit"}>Login</button>
+    </LoginWrapper>
+  );
 };
+
+const LoginWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 5rem;
+`;
