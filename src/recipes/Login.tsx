c9810be70f1b56e14ece01f9438f4e9987fb644e
osund <osundal83@gmail.com>
2020-08-12 22:32:48 +0200

    frontend now receives jwt and stores it in localstorage upon successful login

--- src/recipes/Login.tsx
+++ src/recipes/Login.tsx
@@ -1,26 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { GLOBAL_API } from "../common/constants";
+import { useHistory } from "react-router-dom";
 
-export const Login = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+interface ILogin {
+  token: {};
+  setToken: React.Dispatch<React.SetStateAction<{}>>;
+}
+
+export const Login: React.FC<ILogin> = ({ token, setToken }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { goBack }: any = useHistory();
 
   const submitForm = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // TODO: create backend endpoint and do auth
-    console.log("clicked form submit");
+    const payload = {
+      username,
+      password
+    };
+
+    const url = `${GLOBAL_API}users/login`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      // return user if login success
+      if (response) {
+        const { token, username } = await response.json();
+
+        if (token) {
+          setToken(token);
+          localStorage.setItem("userData", JSON.stringify({ token }));
+          goBack();
+        }
+      }
+    } catch (e) {
+      console.log("could not login ", e.toString());
+    }
   };
 
   return (
     <LoginWrapper onSubmit={submitForm}>
-      Login screen. User is logged in: {isLoggedIn ? "true" : "false"}
+      Login screen. User is logged in: {token ? "true" : "false"}
       <label>
         Username
-        <input />
+        <input
+          type={"text"}
+          value={username}
+          onChange={v => setUsername(v.target.value)}
+        />
       </label>
       <label>
         Password
-        <input />
+        <input
+          type={"password"}
+          value={password}
+          onChange={v => setPassword(v.target.value)}
+        />
       </label>
       <button type={"submit"}>Login</button>
     </LoginWrapper>
