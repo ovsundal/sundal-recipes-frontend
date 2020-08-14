be98781d25574e8d6e9b96a9bfc9fa5dc9a4ac19
osund <osundal83@gmail.com>
2020-08-14 23:37:28 +0200

    app now always tries to verify jwt upon launch /refresh. Removes token from localstorage if expired

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,35 @@ import * as React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import { AppRoutes } from "./AppRoutes";
 import { useEffect, useState } from "react";
+import { GLOBAL_API } from "./common/constants";
 
 export const App: React.FC = () => {
   const [token, setToken] = useState("");
 
+  // this app has no logout, so jwt will persist in browser. If valid token, add to state.
+  // If expired then remove it from localstorage
   useEffect(() => {
+    const verifyToken = async (token: string) => {
+      const url = `${GLOBAL_API}users/verifyToken`;
+
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token
+        }
+      });
+
+      if (response.status === 204) {
+        setToken(storedData.token);
+      } else {
+        localStorage.removeItem("userData");
+      }
+    };
+
     const storedData = JSON.parse(localStorage.getItem("userData") || "{}");
 
     if (storedData && storedData.token) {
-      setToken(storedData.token);
+      verifyToken(storedData.token);
     }
   }, []);
 
