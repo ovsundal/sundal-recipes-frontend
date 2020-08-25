3856134c72982cb2d1e0f576ba3136c781c71ebc
osund <osundal83@gmail.com>
2020-08-25 21:01:35 +0200

    add comment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// path is used by heroku for hosting the site
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });app.listen(port);
